3a4577e9ff9acd08575cd559d2b3c1d3fe806084
BigStepper <breezystepper@gmail.com>
2026-04-15 21:18:27 +0300

    fix: remove duplicate labels from JS

--- system-api.js
+++ system-api.js
@@ -9,20 +9,20 @@ async function getVisitorInfo() {
 
         if (data.error) throw new Error("API Limit");
 
-        ipElement.innerHTML = `IP Address: <span style="color: #90A98F; font-weight: bold;">${data.ip}</span>`;
-        locElement.innerHTML = `Location: <span style="color: #90A98F; font-weight: bold;">${data.city}, ${data.country_name}</span>`;
+        ipElement.innerHTML = `<span style="color: #90A98F; font-weight: bold;">${data.ip}</span>`;
+        locElement.innerHTML =`<span style="color: #90A98F; font-weight: bold;">${data.city}, ${data.country_name}</span>`;
 
     } catch (error) {
         // If the first one fails, we try a second one automatically!
         fetch('https://api.ipify.org?format=json')
             .then(res => res.json())
             .then(data => {
-                ipElement.innerHTML = `IP Address: <span style="color: #90A98F; font-weight: bold;">${data.ip}</span>`;
-                locElement.innerHTML = `Location: <span style="color: #90A98F; font-weight: bold;">Djibouti City, Djibouti</span>`;
+                ipElement.innerHTML = `<span style="color: #90A98F; font-weight: bold;">${data.ip}</span>`;
+                locElement.innerHTML = `<span style="color: #90A98F; font-weight: bold;">Djibouti City, Djibouti</span>`;
             })
             .catch(() => {
-                ipElement.innerText = "IP Address: Local Connection";
-                locElement.innerText = "Location: Djibouti (Offline Mode)";
+                ipElement.innerText = "Local Connection";
+                locElement.innerText = "Djibouti (Offline Mode)";
             });
     }
 }
